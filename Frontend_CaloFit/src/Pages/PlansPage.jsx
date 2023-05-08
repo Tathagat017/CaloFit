f49681519c8583ca4b9eeb8932e9493333be2461
Halderpritam123 <halderpritamhalder1@gmail.com>
2023-05-09 03:07:16 +0530

    plan done

--- Frontend_CaloFit/src/Pages/PlansPage.jsx
+++ Frontend_CaloFit/src/Pages/PlansPage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import DashboardPage from "./DashboardPage";
+import Plans from "./Plans";
 
 const PlansPage = () => {
-  return <DashboardPage>PlansPage</DashboardPage>;
+  return <DashboardPage><Plans/></DashboardPage>;
 };
 
 export default PlansPage;
