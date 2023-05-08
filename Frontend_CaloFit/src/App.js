cbaaf751770d4dabd12d12a24669389e649e984c
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-05-08 10:27:36 +0530

    Account Settings Page FrontEnd Completed

--- Frontend_CaloFit/src/App.js
+++ Frontend_CaloFit/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import About from "./Pages/AboutPage";
 import Blog from "./Pages/Blog";
+import AccountSettings from "./Pages/SettingPage";
+import Support from "./Pages/Support";
 
 function App() {
   return <div className="App">
     <h1>Running Frontend CaloFit App : </h1>
-   <Blog/>
+  <AccountSettings/>
   </div>;
 }
 
